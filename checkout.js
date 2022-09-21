bd7f0d58f0f1d8d7c9dcd069b822c6755e231eef
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-09-21 22:02:05 +0300

    2.ders bitti

--- checkout.js
+++ checkout.js
@@ -2,67 +2,76 @@
 // const shippingPrice = 15;
 // const shippingFreePrice = 300;
 
-window.addEventListener("load", () => {
-  //set items to LocalStorage
-  // localStorage.setItem("taxRate", taxRate);
-  // localStorage.setItem("shippingPrice", shippingPrice);
-  // localStorage.setItem("shippingFreePrice", shippingFreePrice);
-  //set items to sessionStorage
-  //  sessionStorage.setItem("taxRate", taxRate);
-  //  sessionStorage.setItem("shippingPrice", shippingPrice);
-  //  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+window.addEventListener("load", ()=>{
+    //set items to LocalStorage
+    // localStorage.setItem("taxRate", taxRate);
+    // localStorage.setItem("shippingPrice", shippingPrice);
+    // localStorage.setItem("shippingFreePrice", shippingFreePrice);
+
+     //set items to sessionStorage
+    //  sessionStorage.setItem("taxRate", taxRate);
+    //  sessionStorage.setItem("shippingPrice", shippingPrice);
+    //  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const productsDiv = document.querySelector(".products");
 //Capturing vs. Bubbling
-productsDiv.addEventListener("click", (event) => {
-  if (event.target.className == "fa-solid fa-minus") {
-    //console.log("minus btn is clicked!");
-    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
-      event.target.parentElement.querySelector(".quantity").innerText--;
-      calculateProductPrice(event.target);
-      calculateCartPrice();
-    } else {
-      if (confirm("Product will be removed???")) {
-        //remove
+productsDiv.addEventListener("click", (event)=>{
+    if(event.target.className == "fa-solid fa-minus"){
+        //console.log("minus btn is clicked!");
+        if(event.target.parentElement.querySelector(".quantity").innerText > 1){
+            event.target.parentElement.querySelector(".quantity").innerText--;
+            calculateProductPrice(event.target);
+            calculateCartPrice();
+        }
+        else{
+            if(confirm("Product will be removed???")){
+                //remove
+                event.target.parentElement.parentElement.parentElement.remove();
+                calculateCartPrice();
+            }
+        }
+    }
+    else if(event.target.classList.contains("fa-plus")){
+        //console.log("plus btn is clicked!");
+        event.target.previousElementSibling.innerText++;
+        calculateProductPrice(event.target);
+        calculateCartPrice();
+    }
+    else if(event.target.className == "remove-product"){
+        //console.log("remove btn is clicked!");
         event.target.parentElement.parentElement.parentElement.remove();
         calculateCartPrice();
-      }
     }
-  } else if (event.target.classList.contains("fa-plus")) {
-    //console.log("plus btn is clicked!");
-    event.target.previousElementSibling.innerText++;
-    calculateProductPrice(event.target);
-    calculateCartPrice();
-  } else if (event.target.className == "remove-product") {
-    //console.log("remove btn is clicked!");
-    event.target.parentElement.parentElement.parentElement.remove();
-    calculateCartPrice();
-  } else {
-    //console.log("other element is clicked!");
-  }
+    else{
+        //console.log("other element is clicked!");
+    }
 });
 
-const calculateProductPrice = (btn) => {
-  const productInfoDiv = btn.parentElement.parentElement;
-  //console.log(productInfoDiv);
-  const price = productInfoDiv.querySelector(".product-price strong").innerText;
-  const quantity = productInfoDiv.querySelector(".quantity").innerText;
-  const productTotalDiv = productInfoDiv.querySelector(".product-line-price");
-  productTotalDiv.innerText = (price * quantity).toFixed(2);
-};
+const calculateProductPrice = (btn) =>{
+    const productInfoDiv = btn.parentElement.parentElement;
+    //console.log(productInfoDiv);
+    const price = productInfoDiv.querySelector(".product-price strong").innerText;
+    const quantity = productInfoDiv.querySelector(".quantity").innerText;
+    const productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+    productTotalDiv.innerText = (price * quantity).toFixed(2);
+    //alert(quantity);
 
-const calculateCartPrice = () => {
-  const productTotalPricesDivs =
-    document.querySelectorAll("product-line-price");
-  let subTotal = 0;
-  productTotalPricesDivs.forEach((div) => {
-    subTotal += parseFloat(div.innerText);
-  });
-  const taxPrice = subTotal * localStorage.getItem("taxRate");
-  const shippingPrice =
-    subTotal > 0 && subTotal < localStorage.getItem("shippingFreePrice")
-      ? localStorage.getItem("shippingPrice")
-      : 0;
-  console.log(shippingPrice);
-};
+}
+
+const calculateCartPrice = () =>{
+    const productsTotalPricesDivs = document.querySelectorAll(".product-line-price");
+    //foreach ==> NodeList, Array
+    //const productsTotalPricesDivs = [...document.getElementsByClassName("product-line-price")];
+
+    let subtotal = 0;
+    productsTotalPricesDivs.forEach(div =>{
+        subtotal += parseFloat(div.innerText);
+    });
+    //console.log(subtotal);
+    const taxPrice = subtotal * localStorage.getItem("taxRate");
+
+    const shippingPrice = (subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice") ? localStorage.getItem("shippingPrice") : 0)
+
+    console.log(shippingPrice);
+}S
